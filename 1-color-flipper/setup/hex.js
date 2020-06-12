a1ae7073e630c07496e9c2938893270dd8643dd8
Deepti Boddapati <deeptiboddapati@gmail.com>
2020-06-12 17:08:38 -0500

    Solution to color flipper.

--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const textIndicator = document.getElementsByClassName('color')[0];
+const backgroundElement = document.getElementsByTagName('main')[0];
+
+var btn = document.getElementById('btn');
+btn.addEventListener('click',changeColor);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function changeColor(event) {
+	var color = '#';
+	for (var i = 5; i >= 0; i--) {
+		color += hex[getRandomInt(16)];
+	}
+	backgroundElement.setAttribute('style', "background-color:"+ color +";");
+
+	textIndicator.textContent = color;
+}
