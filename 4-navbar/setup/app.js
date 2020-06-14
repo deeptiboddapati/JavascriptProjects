2bf8446680f4c2d5876b7c92b594cefc8ef42e06
Deepti Boddapati <deeptiboddapati@gmail.com>
2020-06-14 18:05:51 -0500

    Project 4 solution

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -1,5 +1,9 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+const button = document.getElementsByClassName('nav-toggle')[0];
+const links = document.getElementsByClassName('links')[0];
+
+button.addEventListener( 'click', toggleNavLinks );
+
+
+function toggleNavLinks(event) {
+	links.classList.toggle('collapsed');
+}
